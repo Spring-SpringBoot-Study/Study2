86bff41f10f00ec2a36bbfe7356c59f3a4382a0f
ChaeHyunLim <chaehyun010104@gmail.com>
2026-03-12 20:18:38 +0900

    section4 : Spring으로 전환하기

--- src/main/java/com/example/spring_study/spring_study/MemberApp.java
+++ src/main/java/com/example/spring_study/spring_study/MemberApp.java
@@ -3,12 +3,19 @@ package com.example.spring_study.spring_study;
 import com.example.spring_study.spring_study.member.Grade;
 import com.example.spring_study.spring_study.member.Member;
 import com.example.spring_study.spring_study.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+        // AppConfig appConfig = new AppConfig();
         // 기존 MemberService memberService = new MemberServiceImpl()을 대체 -> AppConfig의 생성자로 memberService 객체를 가져옴
-        MemberService memberService = appConfig.memberService();
+        // MemberService memberService = appConfig.memberService();
+
+        // Spring에서는 ApplicationContext가 Spring Container임
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
