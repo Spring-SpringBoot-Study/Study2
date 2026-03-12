3fbd042b537923be7e31be5e92e9e0d7013b3f52
ChaeHyunLim <chaehyun010104@gmail.com>
2026-03-12 19:57:58 +0900

    section4 : AppConfig 리팩터링

--- src/main/java/com/example/spring_study/spring_study/AppConfig.java
+++ src/main/java/com/example/spring_study/spring_study/AppConfig.java
@@ -1,6 +1,9 @@
 package com.example.spring_study.spring_study;
 
+import com.example.spring_study.spring_study.discount.DiscountPolicy;
 import com.example.spring_study.spring_study.discount.FixDiscountPolicy;
+import com.example.spring_study.spring_study.discount.RateDiscountPolicy;
+import com.example.spring_study.spring_study.member.MemberRepository;
 import com.example.spring_study.spring_study.member.MemberService;
 import com.example.spring_study.spring_study.member.MemberServiceImpl;
 import com.example.spring_study.spring_study.member.MemoryMemberRepository;
@@ -10,12 +13,21 @@ import com.example.spring_study.spring_study.order.OrderService;
 // 객체의 생성과 연결을 담당함
 public class AppConfig {
 
+    // 각 메서드의 역할이 잘 들어나게 리팩터링
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        // OrderSe
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        // 이 부분만 바꾸면, 할인 방식을 바꿀 수 있음(사용 영역의 코드는 바꿀 필요 없이, 구성 영역의 코드만 바꾸면 됨)
+        return new RateDiscountPolicy();
     }
 }
