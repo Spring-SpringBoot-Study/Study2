666fa3fde345a56fe8eb3e63347e03f38c975307
ChaeHyunLim <chaehyun010104@gmail.com>
2026-03-15 22:25:32 +0900

    section 7 : 중복 등록과 충돌

--- src/main/java/com/example/spring_study/AutoAppConfig.java
+++ src/main/java/com/example/spring_study/AutoAppConfig.java
@@ -1,5 +1,7 @@
 package com.example.spring_study; // AutoAppConfig의 pacakge
 
+import com.example.spring_study.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -20,5 +22,14 @@ import org.springframework.context.annotation.FilterType;
 )
 // @Component 라고 붙은 것을 모두 찾아서 자동으로 Spring Bean으로 등록해줌
 public class AutoAppConfig {
+
+    // MemoryMemberRepository에서 ComponentScan으로 자동으로 이미 Bean에 등록되었는데, 수동으로 등록하면 충돌이 날까??
+    // 자동 vs 자동, 즉 componentScan끼리의 충돌(Bean의 이름이 같음)은 에러가 남
+    // 수동 vs 자동의 경우, 수동이 우선권을 가짐(수동 Bean이 자동 Bean을 오버라이딩함) -> 에러가 안남
+    // 최근 Spring Boot에서는 오류가 나도록 바뀜(properties에서 설정으로 오버라이딩되게 변경 가능)
+    @Bean(name = "memoryMemberRepository")
+    MemoryMemberRepository memoryMemberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 }
