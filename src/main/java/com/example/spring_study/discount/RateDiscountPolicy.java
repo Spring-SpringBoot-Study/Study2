41686800b9c009850c786fa71916f04ecdb1945e
ChaeHyunLim <chaehyun010104@gmail.com>
2026-03-20 19:38:05 +0900

    section 8 : 조회 빈이 2개 이상 - 문제 -> @Qualifier vs @Primary

--- src/main/java/com/example/spring_study/discount/RateDiscountPolicy.java
+++ src/main/java/com/example/spring_study/discount/RateDiscountPolicy.java
@@ -2,9 +2,14 @@ package com.example.spring_study.discount;
 
 import com.example.spring_study.member.Grade;
 import com.example.spring_study.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+// @Qualifier("mainDiscountPolicy")
+@Primary // -> Bean 타입이 겹칠 경우, 최상위 우선순위를 가지게 함
+// @Qualifire vs @Primary 의 선택이지만, 둘 중에서 우선순위는 @Qualifire가 가져감(Spring은 자동보다는 수동이, 넓은 범위보다 좁은 범위의 선택권이 우선순위임)
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int dicountPercent = 10;
--- src/main/java/com/example/spring_study/order/OrderServiceImpl.java
+++ src/main/java/com/example/spring_study/order/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.spring_study.member.Member;
 import com.example.spring_study.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,13 +13,20 @@ import org.springframework.stereotype.Component;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
+
+    // @Qualifier("mainDiscountPolicy")
+    // 이처럼 Lombock에서도 @Qualifier 사용 가능(main/java/lombok.config에서 설정 추가 필요)
     private final DiscountPolicy discountPolicy;
 
     // 생성자 주입 방식을 사용하면, final 키워드를 사용할 수 있다.
     // final은 선언 직후에 바로 초기화가 이루어져야하므로, 개발자가 실수 생성자 코드 미작성 시에 컴파일 오류로 식별 가능
     // 생성자가 1개이므로, @Autowired 생략 가능
     // @RequiredArgsConstructor으로 아래의 생성자를 자동으로 만들어줌
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+
+    // 같은 타입(DiscountPolicy) 2개가 @Component로 등록되어 있는데, 어떤 것을 꺼내야와야할지 몰라서 에러가 발생하는 것우
+    // @Qualifier 를 이용해서 Bean을 지정 가능(Bean의 이름이 바뀌는 것은 아니다)
+    // 이 예시에서는 RateDiscountPolicy.java에서 @Qualifier("mainDiscountPolicy")가 붙어 있어서 RateDiscountPolicy를 의존성 주입해줌
+//    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
 //        this.memberRepository = memberRepository;
 //        this.discountPolicy = discountPolicy;
 //    }
