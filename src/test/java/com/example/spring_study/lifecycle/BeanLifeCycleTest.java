3965d3da20ba42d81673620cebf358ed0d8a4374
ChaeHyunLim <chaehyun010104@gmail.com>
2026-03-24 15:39:37 +0900

    section 9 : 애노테이션 @PostConstruct, @PreDestroy

--- src/test/java/com/example/spring_study/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/com/example/spring_study/lifecycle/BeanLifeCycleTest.java
@@ -19,13 +19,14 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean(initMethod = "init", destroyMethod = "close") // NetWorkClient의 메소드 중에서 초기화와 종료 메소드를 지정
+        // @Bean(initMethod = "init", destroyMethod = "close") // NetWorkClient의 메소드 중에서 초기화와 종료 메소드를 지정
         // -> 코드가 아니라 설정 정보를 사용하기 때문에 코드를 고칠 수 없는 외부 라이브러리에도 초기화, 종료 메서드를 적용할 수 있음
         // Bean이 완전히 등록된 후에 "init()이 호출됨
 
         // destroyMethod는 (inferred) 추론으로 등록되어 있어서, 종료 메서드는 따로 적어주지 않아도 @Bean으로 등록할 시에는 'close`, 'shutdown' 라는 이름의 메서드를 자동으로 호출해줌
         // -> 즉, 이름 그대로 종료 메서드를 추론해서 호출(일반적으로 외부라이브러리의 경우, close, shudown을 종료 메서드 이름임)
         // -> 이 추론 기능을 사용하기 싫으면 destroyMethod = ""로 지정하면 됨
+        @Bean
         public NetworkClient networkClient() {
             // 객체의 생성과 초기화는 분리하는 것이 좋다
             // 생성자는 필수 정보(파라미터)를 받고, 메모를 할당해서 객체를 생성하는 책임을 가짐
--- src/test/java/com/example/spring_study/lifecycle/NetworkClient.java
+++ src/test/java/com/example/spring_study/lifecycle/NetworkClient.java
@@ -1,7 +1,7 @@
 package com.example.spring_study.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 public class NetworkClient {
 
@@ -30,6 +30,8 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    // 이 방법은 외부 라이브러리에는 적용하지 못함
+    @PostConstruct // 생성자 이후에 호출한다는 뜻
     public void init() throws Exception { // 의존관계 주입이 끝나면 호출해 주겠다는 뜻
         // 생성자에서는 진짜 생성만 하고, 초기화하는 과정을 분리함
         System.out.println("NetworkClient.init");
@@ -37,6 +39,7 @@ public class NetworkClient {
         call("초기화 생성 메시지");
     }
 
+    @PreDestroy // 종료 이전에 호출한다는 뜻
     public void close() throws Exception { // Bean이 종료될 때 호출
         System.out.println("NetworkClient.close");
         disconnect();
